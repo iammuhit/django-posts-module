a86a9ec1ea3b5680cda0f2863dc34b88a8663f50
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2025-12-28 01:40:34 +0100

    create post model

--- models.py
+++ models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+class Post(models.Model):
+    author       = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title        = models.CharField(max_length=200)
+    content      = models.TextField()
+    published_at = models.DateTimeField(blank=True, null=True)
+    created_at   = models.DateTimeField(default=timezone.now)
+    updated_at   = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('posts.view', kwargs={'pk': self.pk})
+    
+    def publish(self):
+        self.published_at = timezone.now()
+        self.save()
+
+    def comments(self):
+        return self.comments.filter(approved=True)
