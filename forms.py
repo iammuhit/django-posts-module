ff6dfadd682db9ee86fcf64438ae4f4bf2a0d200
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2025-12-28 02:00:39 +0100

    create post-form class

---
+++ forms.py
@@ -0,0 +1,13 @@
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model  = Post
+        fields = ('author', 'title', 'content')
+
+        widgets = {
+            'title'  : forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control editable medium-editor-textarea', 'rows': 10}),
+        }
