198735dc586891f985453b37c2e5cad50ee1c2fe
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2025-12-29 08:38:38 +0100

    update urls order

--- urls.py
+++ urls.py
@@ -6,8 +6,8 @@ app_name = 'app.modules.posts'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='posts.index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='posts.view'),
     path('create/', views.CreateView.as_view(), name='posts.create'),
+    path('<int:pk>/', views.DetailView.as_view(), name='posts.view'),
     path('<int:pk>/edit/', views.UpdateView.as_view(), name='posts.edit'),
     path('<int:pk>/delete/', views.DeleteView.as_view(), name='posts.delete'),
     path('<int:pk>/publish/', views.publish, name='posts.publish'),
