372915e2a6e7cdf22514555769bc73ecce7b7b9f
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2025-12-29 04:37:12 +0100

    create post views, templates, and urls

--- urls.py
+++ urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from . import views
+from app.modules.posts import views
 
 app_name = 'app.modules.posts'
 
 urlpatterns = [
-    path('', views.index, name='posts.index'),
+    path('', views.IndexView.as_view(), name='posts.index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='posts.view'),
+    path('create/', views.CreateView.as_view(), name='posts.create'),
+    path('<int:pk>/edit/', views.UpdateView.as_view(), name='posts.edit'),
+    path('<int:pk>/delete/', views.DeleteView.as_view(), name='posts.delete'),
+    path('<int:pk>/publish/', views.publish, name='posts.publish'),
+    path('drafts/', views.DraftsView.as_view(), name='posts.drafts'),
 ]
--- views.py
+++ views.py
@@ -1,5 +1,72 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.utils import timezone
+from django.views import generic
 
-def index(request):
-    return render(request, 'posts/index.html')
+from app.modules.posts import REDIRECT_FIELD_NAME
+from app.modules.posts.forms import PostForm
+from app.modules.posts.models import Post
+
+
+class IndexView(generic.ListView):
+    model = Post
+    template_name = 'posts/index.html'
+    context_object_name = 'posts'
+    
+    def get_queryset(self):
+        return Post.objects.filter(published_at__lte=timezone.now()).order_by('-published_at')
+
+
+class DetailView(generic.DetailView):
+    model = Post
+    template_name = 'posts/view.html'
+
+
+class CreateView(LoginRequiredMixin, generic.CreateView):
+    model = Post
+    form_class = PostForm
+    login_url = settings.LOGIN_URL
+    redirect_field_name = REDIRECT_FIELD_NAME
+    template_name = 'posts/form.html'
+
+
+class UpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Post
+    form_class = PostForm
+    login_url = settings.LOGIN_URL
+    redirect_field_name = REDIRECT_FIELD_NAME
+    template_name = 'posts/form.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs.setdefault('template', {
+            'title': 'Update Post',
+        })
+        return super().get_context_data(**kwargs)
+
+
+class DeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Post
+    success_url = reverse_lazy('app.modules.posts:posts.index')
+    template_name = 'posts/delete-confirmation.html'
+
+
+class DraftsView(LoginRequiredMixin, generic.ListView):
+    model = Post
+    login_url = settings.LOGIN_URL
+    redirect_field_name = REDIRECT_FIELD_NAME
+    template_name = 'posts/drafts.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(published_at__isnull=True).order_by('created_at')
+
+
+@login_required(login_url=settings.LOGIN_URL)
+def publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+
+    return redirect('app.modules.posts:posts.view', pk=pk)
