fdb2ab6678c166a8531183870acee7fa6975c1cc
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2025-12-30 18:01:47 +0100

    add category model to admin

--- admin.py
+++ admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.modules.posts.models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'created_at', 'updated_at')
+
+admin.site.register(Category, CategoryAdmin)
