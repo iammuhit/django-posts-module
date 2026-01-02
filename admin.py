3ee2f3a99ef742f39c428ba92491dd146305a650
Nurul Amin Muhit <nurulaminmuhit@gmail.com>
2026-01-02 02:35:46 +0100

    update admin site registrations

--- admin.py
+++ admin.py
@@ -3,14 +3,14 @@ from django.contrib import admin
 from app.modules.posts.models import Category, Post
 
 
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'created_at', 'updated_at')
 
+
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'category__name', 'featured', 'status', 'published_at', 'created_at', 'updated_at')
     search_fields = ('title', 'category__name', 'author__username')
-
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Post, PostAdmin)
